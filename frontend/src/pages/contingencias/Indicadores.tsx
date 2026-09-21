e8dbbc52296a1f7480d5285cb6b3e55038cf2b73
yojhanHuanca <yhuancayucra@gmail.com>
2026-09-21 10:41:46 -0500

    fix: agregar pagina de indicadores de contingencias

---
+++ frontend/src/pages/contingencias/Indicadores.tsx
@@ -0,0 +1,12 @@
+import { ContingenciaShell } from "@/components/layout/ContingenciaShell";
+
+export function Indicadores() {
+  return (
+    <ContingenciaShell>
+      <div>
+        <h1>Indicadores</h1>
+        {/* Tu contenido aquí */}
+      </div>
+    </ContingenciaShell>
+  );
+}
